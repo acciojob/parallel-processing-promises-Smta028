05547f94287b80e7d429be3ca35d3d3dd6b11cb4
Samta <108336676+Smta028@users.noreply.github.com>
2023-04-30 18:54:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 //your JS code here. If required.
-const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
-
 const images = [
   {
     url: "https://picsum.photos/id/237/200/300",
